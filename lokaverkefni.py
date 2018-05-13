097bfef6a008b052b0eb29d7063204f1a97233a6
Eyjolfur00 <eyjolfurar268@nemi.tskoli.is>
2018-05-13 00:57:28 +0000

    Update lokaverkefni.py

--- lokaverkefni.py
+++ lokaverkefni.py
@@ -1,14 +1,31 @@
 from bottle import *
-import os
 import json
 
 @route("/")
 def index():
     return template("Lokaverkefni.tpl")
 
+@route("/stadsetning/<name>")
+def hus(name):
+    return template("hus.tpl",name=name)
+
 @route('/Myndir/<filename>')
 def server_static(filename):
     return static_file(filename, root="./Myndir")
+
+@get("/data")
+def data():
+    nafn = request.query.get("nafn")
+    lykilord = request.query.get("lykilord")
+    nafn = nafn.lower()
+    lykilord = lykilord.lower()
+    if nafn == "admin" and lykilord == "admin":
+        response.set_cookie("admin","ok")
+    else:
+        return template("hus.tpl")
+
+    if request.get_cookie("admin"):
+        return template("admin_site.tpl")
 
 @error(404)
 def error404(error):
